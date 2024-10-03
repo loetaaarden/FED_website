2c9908ab1cff3d90d4302fb5d78da74a46364ce8
loetaaarden <loetaaarden@gmail.com>
2024-10-03 12:40:45 +0200

    commit

--- scripts/scriptIndex.js
+++ scripts/scriptIndex.js
@@ -5,9 +5,10 @@ function VeranderFoto() {
 
   if (scrollPosition > 0.1) { // als er meer dan 10 pixel voegt de class scrolled toe
     imageContainer.classList.add("scrolled"); //voeg class scrolled toe
-  } else {
-    imageContainer.classList.remove("scrolled");
   }
+  //  else {
+  //   imageContainer.classList.remove("scrolled");
+  // }
 }
 
 window.addEventListener("scroll", VeranderFoto); //elke keer als de er gescrolled wordt zal de function uitgevoerd worden
